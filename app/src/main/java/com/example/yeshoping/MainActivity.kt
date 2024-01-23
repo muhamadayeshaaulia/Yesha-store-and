d0b027dea4152f7ae0a5e8ee2f360f4d9892db7b
Lenovo <shaxyyy.03@gmail.com>
2024-01-24 02:19:34 +0700

    first init

---
+++ app/src/main/java/com/example/yeshoping/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.yeshoping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
